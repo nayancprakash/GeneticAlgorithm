de5373fe00bbc34fb23488d7c2b70969c07a8b28
neeraj prakash <neerajprakash@neerajs-MacBook-Air.local>
2018-11-22 08:50:45 -0700

    Skeleton code for Species

--- Species.py
+++ Species.py
@@ -1,5 +1,23 @@
+import numpy
+
 class Species:
 
-    def __init__(self, Shapes):
+    def __init__(self, Shapes, width, height):
         self.shapes = Shapes;
         self.fitness = 0;
+        self.image = numpy.zeros((width, height, 3))
+
+    def drawSpecies(self):
+        for shape in self.shapes:
+            self.image = shape.drawShape(self.image)
+        return self.image
+
+
+    def getFitness(self, img):
+        for x in range(self.image.shape[0]):
+            for y in range(self.image.shape[1]):
+                deltaB = abs(img[x][y][0] - self.image[x][y][0])
+                deltaG = abs(img[x][y][1] - self.image[x][y][1])
+                deltaR = abs(img[x][y][2] - self.image[x][y][2])
+                fitness = deltaB^2 + deltaG^2 + deltaR^2
+                return fitness
