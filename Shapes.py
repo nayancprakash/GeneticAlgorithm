ba99e89deef9c18f50c8dd5143e527b86cfa3591
neeraj prakash <neerajprakash@neerajs-MacBook-Air.local>
2018-11-22 08:40:24 -0700

    Structure Files

---
+++ Shapes.py
@@ -0,0 +1,9 @@
+class Shapes:
+
+    def __init__(self, points):
+        self.points = points
+        self.alpha = 0.00
+        self.color = [0, 0, 0]
+
+    def drawShape(self, img):
+        
---
+++ Species.py
@@ -0,0 +1,5 @@
+class Species:
+
+    def __init__(self, Shapes):
+        self.shapes = Shapes;
+        self.fitness = 0;
